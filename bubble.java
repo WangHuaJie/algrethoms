f53c7b9bf17fb10352f5882c6c2e3119e11b0d20
Harry King <wanghuajie1994@gmail.com>
2014-04-11 15:46:06 +0800

    come on

--- bubble.java
+++ bubble.java
@@ -30,7 +30,7 @@ public class bubble{
 		int sign = 0;
 		for(int i=0;i<arr.length;i++){
 			for(int j=0;j<arr.length-i-1;j++){
-				sign++;                                 //
+				sign++;                                 //signing the cycle index
 				if(arr[j]>arr[j+1]){
 					swap(arr,j,j+1);
 				}
