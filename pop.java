5592209046012c0f37a75b3f91caa3fe8580df01
Harry King <wanghuajie1994@gmail.com>
2014-04-11 13:30:02 +0800

    bubble sorting

---
+++ pop.java
@@ -0,0 +1,59 @@
+public class pop{
+	public static void main(String[] args){
+		int[] arr = new int[10];
+
+		creatArray(arr);
+		System.out.print("create random int array arr[]:            ");
+		showArray(arr);
+		popSort(arr);
+		System.out.print("after pop sorting the random array arr[]: ");
+//		popSortImproved(arr);
+//		System.out.print("after the improving of pop sorting :      ");
+		showArray(arr);
+
+	}
+
+	public static void creatArray(int[] arr){         //create random int array arr[]
+		for(int i=0;i<10;i++){
+			arr[i] = (int)(Math.random()*(101));
+		}
+	}
+
+	public static void showArray(int[] arr){          //print the array arr[] in order
+		for(int i=0;i<10;i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.print("\n");
+	}
+
+	public static void popSort(int[] arr){            //pop sort the random array arr[]
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr.length-i-1;j++){
+				if(arr[j]>arr[j+1]){
+					swap(arr,j,j+1);
+				}
+			}
+		}
+	}
+
+	public static void popSortImproved(int[] arr){           //improving pop sorting
+		boolean needNextPass = true;
+		for(int i=0;i<arr.length && needNextPass;i++){
+			needNextPass = false;
+			for(int j=0;j<arr.length-i-1;j++){
+				if(arr[j]>arr[j+1]){
+					swap(arr,j,j+1);
+					needNextPass = true;
+				}
+			}
+		}
+	}
+
+	public static void swap(int[] arr,int a1,int a2){
+		int tmp;
+		tmp = arr[a1];
+		arr[a1] = arr[a2];
+		arr[a2] = tmp;
+	}
+
+}
